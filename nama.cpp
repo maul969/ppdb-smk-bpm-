3a48a30b1c2cbd8b656476bdfef7ac7e10889f06
alif <maulidana360@gmail.com>
2026-01-22 09:31:51 +0700

    Add age check program in nama.cpp

---
+++ nama.cpp
@@ -0,0 +1,17 @@
+#include<cstdlib>
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"masukkan umur kamu : ";
+    cin>>a;
+
+    if(a<=17){
+        cout<<"kamu belum besar"<<endl;
+
+    } else {
+        cout<<"kamu sudah besaar"<<endl;
+    }
+    system("pause");
+}
